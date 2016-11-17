2fe456c308d9b7f6921d38640ded6d2e348e9a63
ZHuthm <97njczh@gmail.com>
2016-11-17 22:53:52 +0800

    【取消】使用编号选择项目功能；【添加】光标控制选项功能，优化用户体验。

--- menu.h
+++ menu.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <stdio.h> 
+#include <windows.h>
+#include <conio.h>
+#include <stdlib.h> 
+#define OPTIONS 6
+
 
 ///////////////////////////////////////////////////////
 // º¯ÊýÉùÃ÷
